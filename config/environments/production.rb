0da735a3c04d3bf0fc89cefbf85341097814b603
hirobitch <89516353+hirobitch@users.noreply.github.com>
2022-02-13 16:51:15 +0900

    index作成

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "wine_counseling_office_production"
+  # config.active_job.queue_name_prefix = "wine_consultation_office_production"
 
   config.action_mailer.perform_caching = false
 
